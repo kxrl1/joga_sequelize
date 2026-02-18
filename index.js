9343978cdc79d3af65d6e9489f678dd0ce5cbe3c
karl <karl.padu@voco.ee>
2026-02-18 09:02:01 +0200

    Create connection to database

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 
 app.use(express.json());
 
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize');
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to sequelize application' });
 });
